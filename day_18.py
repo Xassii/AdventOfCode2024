cea3552c06cf55ea9852b9c963ca82705b2d1ae9
Xassii <amanda.ivarsson@telia.com>
2025-07-20 09:45:44 +0200

    Day 18 part 2 complete

--- day_18.py
+++ day_18.py
@@ -62,6 +62,33 @@ def find_shortest_path(is_at, been_at, direc, dist, max_dist):
     return max_dist
 
 
+def find_exit(is_at, been_at, direc):# TODO
+    if is_at.type:
+        return True
+    
+    directions = list(is_at.neigbour.keys())
+    if direc:
+        directions.remove(get_opposit_direction(direc))
+    
+    been_at.append(is_at.position)
+    
+    for direction in directions:
+        go_to = is_at.neigbour[direction]
+        if go_to == None:
+            continue
+        if go_to.position == None:
+            continue
+        
+        if not go_to.position in been_at:
+            result = find_exit(go_to, been_at, direction)
+            if result:
+                return result
+    
+    been_at.pop()
+    
+    return False
+
+
 with open('Day18Input.txt') as file:
     instructions = file.readlines()
 
@@ -83,8 +110,6 @@ for y in range(71):
             memory_space[y][x].remember['W'] = 71*71
             memory_space[y][x - 1].remember['E'] = 71*71
 
-# --- Part 1 ---
-
 for i in instruction1:
     instruction = i.split(',')
     x = int(instruction[0])
@@ -94,9 +119,36 @@ for i in instruction1:
     memory_space[y][x] = None
 
 memory_space[70][70].type = True
+
+# --- Part 1 ---
+
 memory_space[0][0].remember['E'] = 0
 memory_space[0][0].remember['S'] = 0
 result = find_shortest_path(memory_space[0][1], [], 'E', 1, 71*71)
 result = find_shortest_path(memory_space[1][0], [], 'S', 1, result)
 
 print(f'Part 1: {result}')
+
+# --- Part 2 ---
+
+for i in instruction2:
+    instruction = i.split(',')
+    x = int(instruction[0])
+    y = int(instruction[1])
+    
+    memory_space[y][x].position = None
+
+instruction2.reverse()
+
+for i in instruction2:
+    instruction = i.split(',')
+    x = int(instruction[0])
+    y = int(instruction[1])
+    
+    memory_space[y][x].position = (x, y)
+    
+    found_exit = find_exit(memory_space[0][0], [], '')
+    
+    if found_exit:
+        print(f'Part 2: {x},{y}')
+        break
